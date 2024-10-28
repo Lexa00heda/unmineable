21f3a0222c843ec2c8727cefa0f53cafa496b332
Lexa00heda <120236068+Lexa00heda@users.noreply.github.com>
2024-10-28 14:49:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./xmrig -a gr -o stratum+ssl://ghostrider.unmineable.com:443 -u BTC:bc1qlh4dpprjt5kzm6zmgj2vpyhvvyhhsey4hhjt3g."$device-$(uptime -p | tr -d ' ')-$(TZ="Asia/Kolkata" date +"%H:%M:%S")"#hxoz-386u -p x
+./xmrig -a gr -o stratum+ssl://ghostrider.unmineable.com:443 -u BTC:bc1qlh4dpprjt5kzm6zmgj2vpyhvvyhhsey4hhjt3g."$device-$(uptime -p | tr -d ' ')-$(TZ="Asia/Kolkata" date +"%H:%M:%S")"#hxoz-386u -p x >> xmrig_output.log 2>&1
